5c5f040aa06f4bf265b9f7448498961b51fc0a12
Thiago Souza <153232481+thiagosouza28@users.noreply.github.com>
2024-05-01 16:58:50 -0300

    main

---
+++ script.js
@@ -0,0 +1,6 @@
+// Esse script irá ocultar a tela de carregamento quando o conteúdo da página estiver completamente carregado
+window.addEventListener('load', function() {
+    var loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'none';
+  });
+  
